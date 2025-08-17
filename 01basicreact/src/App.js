9460f23da97a743676fd03a67f4327e97d0e8e5f
ravindrasawant <ravindrasawant2205@gmail.com>
2025-08-17 11:49:15 +0530

    basic with react and vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>React - basic react</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>from vite - ravi</h1>
+  )
+}
+
+export default App
